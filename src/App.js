9a4b063d32d2128feff9c7acc6c0eb8a827f9e2a
Ryan <snorekr@gmail.com>
2021-10-08 08:56:33 -0400

    Set chatacter state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // Set character state
+  const [character, setCharacter] = useState('')
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  
 
   return (
     <div className="App">
