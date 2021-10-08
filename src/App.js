14f31dfda034113da5b0017b50e7652e4abe6757
Ryan <snorekr@gmail.com>
2021-10-08 09:05:08 -0400

    Added side effect to handle api request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
   // Set character state
   const [character, setCharacter] = useState('')
 
+  // Get api data and sync to character state
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(`Error: ${err}`))
+  }, [character])
   
 
   return (
